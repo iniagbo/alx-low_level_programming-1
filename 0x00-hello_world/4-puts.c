67448673b61bc101272cba707ec33cad25b40d52
Mahmud Affa <affamahmud7@gmail.com>
2022-10-13 21:50:27 -0700

    modified last commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- * */
+ * main - prints the phrase "Programming is like building a multilingual
+ * puzzle"
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
+puzzle");
 	return (0);
-}	
+}
